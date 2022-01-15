473d2cab7ee67421780c71d872a7de0a6c3dd259
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-01-15 22:03:31 +0100

    Added better security with IP handling

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import aiohttp
+import hashlib
 
 from quart import Quart, render_template, request
 
@@ -14,6 +15,13 @@ with open("config.json", "r") as f:
     config = json.load(f)
 
 
+def string_to_sha256(text: str):
+    salt = config.get("sha256_salt", "")
+
+    result = hashlib.sha512(f"{salt}{text}".encode()).hexdigest()
+    return result
+
+
 @app.route("/")
 async def index():
     ip_for_seed = request.headers.get("CF-Connecting-IP") or request.headers.get("X-Forwarded-For") or request.remote_addr
@@ -23,6 +31,16 @@ async def index():
             data = await response.json()
 
     return await render_template("index.html", nft=data)
+
+
+@app.route("/test_error")
+async def test_error():
+    return await render_template("error_handler.html")
+
+
+@app.errorhandler(Exception)
+async def handle_exception(e):
+    return await render_template("error_handler.html")
 
 
 if __name__ == "__main__":
