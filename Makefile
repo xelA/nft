8719dda85d9bd2c15093ed12228d7baf60f5941a
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2025-06-08 03:07:11 +0200

    Upgrade to UV and better code

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
-target:
-	@echo hello world lol
+APP_NAME = NFT
+PRISMA_SCHEME = ./schema.prisma
 
-update:
+target:
+	@awk -F ':|##' '/^[^\t].+?:.*?##/ { printf "\033[0;36m%-15s\033[0m %s\n", $$1, $$NF }' $(MAKEFILE_LIST)
+
+git_pull:  ## Pull the latest code from git
 	git pull
-	pm2 restart pm2.json
+
+pm2_start:  ## Create a PM2 instance
+	pm2 start uv --name $(APP_NAME) --interpreter none -- run index.py -u
+
+pm2_restart:  ## Restart PM2
+	pm2 restart $(APP_NAME)
+
+update: git_pull pm2_restart  ## Pull and reboot PM2
--- index.py
+++ index.py
@@ -5,43 +5,50 @@ import aiohttp
 import random
 import hashlib
 
+from dotenvplus import DotEnv
 from io import BytesIO
 from quart import Quart, render_template, request, send_file
 
 app = Quart(__name__)
+config = DotEnv(".env")
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-
-def load_json(filename: str) -> dict:
-    with open(filename, "r", encoding="utf8") as f:
-        data = json.load(f)
-    return data
+with open("./data/testimonials.json", encoding="utf8") as f:
+    testimonials = json.load(f)
 
 
-def string_to_sha512(text: str):
-    salt = config.get("sha512_salt", "")
+def string_to_sha512(text: str) -> str:
+    """
+    Convert a string to a SHA512 hash.
 
-    result = hashlib.sha512(f"{salt}{text}".encode()).hexdigest()
-    return result
+    Parameters
+    ----------
+    text:
+        The string to hash.
 
-
-config = load_json("./config.json")
-testimonials = load_json("./data/testimonials.json")
+    Returns
+    -------
+        The SHA512 hash of the string.
+    """
+    salt = config["CRYPTO_SALT"]
+    return hashlib.sha512(f"{salt}{text}".encode()).hexdigest()
 
 
 @app.route("/")
-async def index():
-    ip_for_seed = request.headers.get("CF-Connecting-IP") or \
-        request.headers.get("X-Forwarded-For") or \
-        request.remote_addr
+async def _index():
+    ip_for_seed: str = (
+        request.headers.get("CF-Connecting-IP") or
+        request.headers.get("X-Forwarded-For") or
+        request.remote_addr or
+        "127.0.0.1"
+    )
 
     encrypted_ip = string_to_sha512(ip_for_seed)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.alexflipnote.dev/nft?seed={encrypted_ip}") as response:
-            data = await response.json()
+    async with aiohttp.ClientSession() as session, session.get(f"https://api.alexflipnote.dev/nft?seed={encrypted_ip}") as response:
+        data = await response.json()
 
     random.shuffle(testimonials)
 
@@ -53,9 +60,9 @@ async def index():
 
 
 async def fetch_and_read(filename: str, text_hex: str, text_colour: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.alexflipnote.dev/nft/{text_hex}/{text_colour}") as response:
-            data = await response.read()
+    """ Fetch a file from the API and return it. """
+    async with aiohttp.ClientSession() as session, session.get(f"https://api.alexflipnote.dev/nft/{text_hex}/{text_colour}") as response:
+        data = await response.read()
 
     return await send_file(
         BytesIO(data), mimetype="image/png",
@@ -65,22 +72,23 @@ async def fetch_and_read(filename: str, text_hex: str, text_colour: str):
 
 
 @app.route("/download/<text_hex>/<text_colour>")
-async def index_download(text_hex, text_colour):
+async def _index_download(text_hex, text_colour):
     return await fetch_and_read("xela_nft.png", text_hex, text_colour)
 
 
 @app.route("/steal/<text_hex>/<text_colour>")
-async def index_stolen_download(text_hex, text_colour):
+async def _index_stolen_download(text_hex, text_colour):
     return await fetch_and_read("stolen_xela_nft.png", text_hex, text_colour)
 
 
 @app.errorhandler(Exception)
-async def handle_exception(e):
+async def _handle_exception(e):
     return await render_template(
         "error_handler.html",
         error_code=e.code,
         error_message=e.description
     )
+
 
 sass.compile(
     dirname=["./static/sass", "./static/css"],
@@ -89,6 +97,6 @@ sass.compile(
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.run(
-    port=config.get("port", 8080),
-    debug=config.get("debug", False)
+    port=config["HTTP_PORT"],
+    debug=config["HTTP_DEBUG"]
 )
